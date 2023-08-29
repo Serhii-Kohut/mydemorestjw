169bee0e397db63549977d20fb709be582fc3501
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2023-08-29 22:11:06 +0300

    UserService - updated createNewUser
    AuthController - updated

--- src/main/java/com/serhii/restjwtproject/demo/service/UserService.java
+++ src/main/java/com/serhii/restjwtproject/demo/service/UserService.java
@@ -1,5 +1,6 @@
 package com.serhii.restjwtproject.demo.service;
 
+import com.serhii.restjwtproject.demo.dtos.RegistrationUserDto;
 import com.serhii.restjwtproject.demo.entities.User;
 import com.serhii.restjwtproject.demo.repositories.RoleRepository;
 import com.serhii.restjwtproject.demo.repositories.UserRepository;
@@ -8,6 +9,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +22,7 @@ import java.util.stream.Collectors;
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public Optional<User> findByUsername(String username) {
         return userRepository.findByUsername(username);
@@ -41,8 +44,12 @@ public class UserService implements UserDetailsService {
         );
     }
 
-    public void createNewUser(User user){
+    public User createNewUser(RegistrationUserDto registrationUserDto) {
+        User user = new User();
+        user.setUsername(registrationUserDto.getUsername());
+        user.setEmail(registrationUserDto.getEmail());
+        user.setPassword(passwordEncoder.encode(registrationUserDto.getPassword()));
         user.setRoles(List.of(roleRepository.findByName("ROLE_USER").get()));
-        userRepository.save(user);
+        return userRepository.save(user);
     }
 }
